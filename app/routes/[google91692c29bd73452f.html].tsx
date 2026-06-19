b809c4cdbf0e9ab87a0f0c40718cf710ab069bda
nabbeshop <nabbeshop@gmail.com>
2026-06-19 14:12:57 +0530

    Update [google91692c29bd73452f.html].tsx

--- app/routes/[google91692c29bd73452f.html].tsx
+++ app/routes/[google91692c29bd73452f.html].tsx
@@ -1,5 +1,8 @@
 export async function loader() {
   return new Response('google-site-verification: google91692c29bd73452f.html', {
-    headers: {'Content-Type': 'text/html'},
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
   });
 }
